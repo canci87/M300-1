03ae42f6647044cff6d39b15c0548a95035e3b19
hunger96 <50396612+hunger96@users.noreply.github.com>
2019-05-08 11:32:46 +0200

    Add files via upload

---
+++ php/www/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>
+
+<?php
+mysqli_connect("db", "root", "hunter2") or die(mysqli_error());
+echo "MySQL Server Verbunden<br />";
+?>
+
+</h1>
+<p>Webserver Lucas Gamper TEST</p>
+
+</body>
+</html>
